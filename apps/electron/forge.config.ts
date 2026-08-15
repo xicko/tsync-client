452e468237f279296f9f9fd744eaceb9ba6abc1d
xicko <gfieldmn@gmail.com>
2026-08-16 00:19:31 +0800

    (feat): add macos/windows icon

--- apps/electron/forge.config.ts
+++ apps/electron/forge.config.ts
@@ -11,6 +11,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: '../../packages/core/assets/icon',
     extendInfo: {
       LSMinimumSystemVersion: '12.0',
     },
